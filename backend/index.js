559a47cc4fc3a76d56a2d8e30b7c422ed36c925b
Kishor1703 <pskishor196@gmail.com>
2025-08-09 10:31:51 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -4,17 +4,27 @@ const app = express();
 const canvasRoutes = require('./routes/canvasRoutes');
 const PORT = 5000;
 
-// CORS middleware for all routes
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://canvasbuilder-flax.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204);
-  }
-  next();
-});
+const allowedOrigins = [
+  'http://localhost:3000',
+'https://canvasbuilder-flax.vercel.app/',
+'https://canvas-kappa-six.vercel.app'
+];
+
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
